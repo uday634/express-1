e06bbeb23803b226ffb72bf3dc36d2f1419bf5d8
uday634 <uk8096386837@gmail.com>
2023-09-09 19:53:01 +0530

    add success page

--- routes/contact-us.js
+++ routes/contact-us.js
@@ -8,6 +8,10 @@ const router = express.Router()
 
 router.get('/contact-us', (req, res, next)=>{
     res.sendFile(path.join(rootDir, 'views', 'constUs.html'))
+    
+})
+router.get('/successful',(req, res, next)=>{
+    res.sendFile(path.join(rootDir, 'views', 'success.html'))
 })
 
 module.exports = router
